dc4e2ae4c61ded37a1942d1c37419e88972c66e3
Raven Crow <rxn2555@gmail.com>
2024-05-20 13:00:41 +0900

    APIキー

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 import flet as ft
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+api_key = config['DEFAULT']['API_KEY']
 
 def main(page):
     page.add(ft.Text('Hello'))
